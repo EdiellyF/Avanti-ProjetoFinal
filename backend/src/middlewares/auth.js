bd0c9f953f623c1006419374593bb13fd5a942ef
joaoev <joaoalvesxp@alu.ufc.br>
2025-04-11 21:19:37 -0300

    fix: middleware auth tinha um erro na função isUser em req.originalUrl

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -7,50 +7,52 @@ export function authMiddleware(request, response, next) {
     const { authorization } = request.headers;
 
     if (!authorization) {
-      return  sendErrorResponse(response, 401,"Unauthorized: No token provided" )
+      return sendErrorResponse(
+        response,
+        401,
+        "Unauthorized: No token provided"
+      );
     }
 
     const token = authorization.replace("Bearer ", "").trim();
 
-
-
-    const { id, role} = jwt.verify(token, process.env.JWT_SECRET);
+    const { id, role } = jwt.verify(token, process.env.JWT_SECRET);
 
     if (!id) {
       return sendErrorResponse(response, 401, "Invalid token");
     }
 
-    request.user = { id, role};
+    request.user = { id, role };
 
     return next();
   } catch (error) {
     console.error("Error in authMiddleware:", error);
-      return sendErrorResponse(response, 401,"Unauthorized: Invalid token" )
+    return sendErrorResponse(response, 401, "Unauthorized: Invalid token");
   }
-
-
-  
-
 }
-  export function isAdminMiddleware(req, res, next) {
-
-    if (req.user?.role !== "ADMIN") {
-      return sendErrorResponse(response, 403, "Apenas administradores podem realizar esta ação." )
-    }
-    next();
+export function isAdminMiddleware(req, res, next) {
+  if (req.user?.role !== "ADMIN") {
+    return sendErrorResponse(
+      response,
+      403,
+      "Apenas administradores podem realizar esta ação."
+    );
   }
+  next();
+}
 
+export function isUser(req, res, next) {
+  const { id } = req.params;
+  const userId = req.user.id;
 
-  export  function isUser(req, res, next) {
-
-    const {id } = req.params; 
-    const userId = req.user.id;
-
-    if(req.OriginalUrl.includes("/users")){
-      if(!(userId == id || req.user?.role == "ADMIN")){
-        return res.status(403).json({ message: "Você só pode acessar ou atualizar seu próprio perfil" }); 
-      }
+  if (req.originalUrl.includes("/users")) {
+    if (!(userId == id || req.user?.role == "ADMIN")) {
+      return res
+        .status(403)
+        .json({
+          message: "Você só pode acessar ou atualizar seu próprio perfil",
+        });
     }
-    next();
   }
-
+  next();
+}
