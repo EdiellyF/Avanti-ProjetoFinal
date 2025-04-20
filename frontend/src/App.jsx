01ee3d11b3f9aa9f0d1d6e768b6cb86d00f1caab
Ediellyf <ediellyferreiradecarvalho@gmail.com>
2025-04-20 12:56:48 -0300

    feat(cadastrarPage): adicionando pagina de cadastro de usuario

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPages from './pages/LoginPages';
+import { SignPage } from './pages/SignPage';
 
 function App() {
   return (
-    <div className="app">
-      <LoginPages />
-    </div>
+
+    <Router>
+        <Routes>
+          <Route path="/" element={<LoginPages />}>  </Route>
+          <Route path="/cadastrar" element={<SignPage/>}> </Route>
+        </Routes>
+    </Router>
+   
+     
+
+
   );
 }
 
