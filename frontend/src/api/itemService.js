a6b355c04df71ef847bc435dc08d3d8a36fd324f
Vitor Loula <vitorloula3@gmail.com>
2025-04-22 12:57:26 -0300

    feat: itemService para rotas para a api

---
+++ frontend/src/api/itemService.js
@@ -0,0 +1,26 @@
+import api from './axios';
+
+export const getItems = async () => {
+  const res = await api.get('/item');
+  return res.data;
+};
+
+export const getItemById = async (id) => {
+  const res = await api.get(`/item/${id}`);
+  return res.data;
+};
+
+export const createItem = async (itemData) => {
+  const res = await api.post('/item', itemData);
+  return res.data;
+};
+
+export const updateItem = async (id, itemData) => {
+  const res = await api.put(`/item/${id}`, itemData);
+  return res.data;
+};
+
+export const deleteItem = async (id) => {
+  const res = await api.delete(`/item/${id}`);
+  return res.data;
+};
