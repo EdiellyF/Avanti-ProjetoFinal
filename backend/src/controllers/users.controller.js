880b17f049e709ba8c62226ff02500e6c0cfd164
Vitor Loula <vitorloula3@gmail.com>
2025-04-23 13:53:21 -0300

    "Refatora métodos do UserController e adiciona validações para criação, atualização e exclusão de usuários"

--- backend/src/controllers/users.controller.js
+++ backend/src/controllers/users.controller.js
@@ -1,23 +1,23 @@
-import { NotFoundError, ValidationError } from "../utils/customErrors.js";
-import { sendErrorResponse } from "../middlewares/sendErrorResponse.js";
-import { isValidUUID } from "../utils/isValidUUID.js";
+import { NotFoundError } from "../utils/customErrors.js"
+import { sendErrorResponse } from "../middlewares/sendErrorResponse.js"
+import { isValidUUID } from "../utils/isValidUUID.js"
 
 export class UserController {
   constructor(userService) {
-    this.userService = userService;
+    this.userService = userService
   }
   async createUser(req, res) {
     try {
-      const { name, email, phone, password } = req.body;
+      const { name, email, phone, password } = req.body
 
       if (!name || !email || !phone || !password) {
-        return res.status(400).json({ message: "All fields are required" });
+        return res.status(400).json({ message: "All fields are required" })
       }
 
-      const existingUser = await this.userService.getUserByEmail(email);
+      const existingUser = await this.userService.getUserByEmail(email)
 
       if (existingUser) {
-        return res.status(400).json({ message: "Email already exists" });
+        return res.status(400).json({ message: "Email already exists" })
       }
 
       const newUser = await this.userService.createUser({
@@ -25,124 +25,123 @@ export class UserController {
         email,
         phone,
         password,
-      });
+      })
 
-      return res.status(201).json(newUser);
+      return res.status(201).json(newUser)
     } catch (error) {
-      console.error("Error creating user:", error);
+      console.error("Error creating user:", error)
       return res.status(500).json({
         message: "Internal server error",
-      });
+      })
     }
   }
 
   async getUserById(req, res) {
     try {
-      const { id } = req.params;
+      const { id } = req.params
+      console.log(`Buscando usuário com ID: ${id}`)
 
-      const user = await this.userService.getUserById(id);
+      const user = await this.userService.getUserById(id)
 
       if (!user) {
-        throw new NotFoundError("User not found ");
+        console.log(`Usuário com ID ${id} não encontrado`)
+        throw new NotFoundError("User not found")
       }
 
-      return res.status(200).json(user);
+      console.log(`Usuário encontrado: ${user.id}, Retornando dados`)
+      return res.status(200).json(user)
     } catch (error) {
-      const statusCode = error.statusCode || 500;
-      return sendErrorResponse(res, statusCode, error.message);
+      console.error("Erro ao buscar usuário:", error)
+      const statusCode = error.statusCode || 500
+      return sendErrorResponse(res, statusCode, error.message)
     }
   }
 
   async login(req, res) {
-    const { email, password } = req.body;
+    const { email, password } = req.body
 
     if (!email || !password) {
-      return res.status(400).json({ message: "All fields are required" });
+      return res.status(400).json({ message: "All fields are required" })
     }
 
     try {
-      const user = await this.userService.getUserByEmail(email);
+      const user = await this.userService.getUserByEmail(email)
 
       if (!user) {
-        return res.status(400).json({ message: "Invalid email or password" });
+        return res.status(400).json({ message: "Invalid email or password" })
       }
 
-      const isPasswordValid = await this.userService.verifyPassword(
-        password,
-        user.password
-      );
+      const isPasswordValid = await this.userService.verifyPassword(password, user.password)
 
       if (!isPasswordValid) {
-        return res.status(400).json({ message: "Invalid email or password" });
+        return res.status(400).json({ message: "Invalid email or password" })
       }
 
-      const token = await this.userService.generateAuthToken(user);
+      const token = await this.userService.generateAuthToken(user)
 
-      return res.status(200).json({ token });
+      return res.status(200).json({ token })
     } catch (error) {
-      console.error("Error logging in:", error);
+      console.error("Error logging in:", error)
 
-      return res.status(500).json({ message: "Internal server error 5" });
+      return res.status(500).json({ message: "Internal server error 5" })
     }
   }
 
   async update(req, res) {
     try {
-      const id = req.params.id;
-      const { email, password } = req.body;
+      const id = req.params.id
+      const { email, password } = req.body
 
       if (!id || (!email && !password)) {
-        return res
-          .status(400)
-          .json({ message: "ID and at least one field are required" });
+        return res.status(400).json({ message: "ID and at least one field are required" })
       }
 
-      const existingUser = await this.userService.getUserByEmail(email);
+      const existingUser = await this.userService.getUserByEmail(email)
 
       if (existingUser) {
-        return res.status(400).json({ message: "Email already exists" });
+        return res.status(400).json({ message: "Email already exists" })
       }
 
       const updatedUser = await this.userService.updateUser({
         id,
         email,
         password,
-      });
+      })
 
-      return res.status(200).json(updatedUser);
+      return res.status(200).json(updatedUser)
     } catch (error) {
-      console.error("Error updating user:", error);
-      return res.status(500).json({ message: "Internal server error" });
+      console.error("Error updating user:", error)
+      return res.status(500).json({ message: "Internal server error" })
     }
   }
 
   async delete(req, res) {
     try {
-      const { id } = req.params;
+      const { id } = req.params
 
       if (!isValidUUID(id)) {
         return res.status(400).json({
           message: "ID não está no formato UUID",
-        });
+        })
       }
 
       if (!id) {
-        return res.status(400).json({ message: "ID is required" });
+        return res.status(400).json({ message: "ID is required" })
       }
 
-      const user = await this.userService.getUserById(id);
+      const user = await this.userService.getUserById(id)
 
       if (!user) {
-        return res.status(404).json({ message: "User not found" });
+        return res.status(404).json({ message: "User not found" })
       }
 
-      await this.userService.deleteUser(id);
+      await this.userService.deleteUser(id)
 
-      return res.status(204).send();
+      return res.status(204).send()
     } catch (error) {
-      console.error("Error deleting user:", error);
-      const statusCode = error.statusCode || 500;
-      return sendErrorResponse(res, statusCode, error.message);
+      console.error("Error deleting user:", error)
+      const statusCode = error.statusCode || 500
+      return sendErrorResponse(res, statusCode, error.message)
     }
   }
 }
