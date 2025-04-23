5f6a363bcbf3c1f63f10291f466c0413b6bd3c71
Vitor Loula <vitorloula3@gmail.com>
2025-04-23 13:53:45 -0300

    Refatora métodos do ItemRepository e adiciona validações para manipulação de dados e relacionamentos

--- backend/src/repositories/item.repository.js
+++ backend/src/repositories/item.repository.js
@@ -1,25 +1,36 @@
-import { prismaClient } from "../database/prismaClient.js";
+import { prismaClient } from "../database/prismaClient.js"
 
 export class ItemRepository {
   async createItem(data) {
-    return await prismaClient.item.create({ data });
+    console.log("Dados recebidos no repositório:", data)
+    try {
+      // Garantir que a data seja um objeto Date válido
+      if (data.data && typeof data.data === "string") {
+        data.data = new Date(data.data)
+      }
+
+      return await prismaClient.item.create({ data })
+    } catch (error) {
+      console.error("Erro no repositório ao criar item:", error)
+      throw error
+    }
   }
 
   async deleteItemById(id) {
-    const item = await this.findByIdItem(id);
+    const item = await this.findByIdItem(id)
 
     if (item) {
       return await prismaClient.item.delete({
         where: { id },
-      });
+      })
     }
-    return null;
+    return null
   }
 
   async findByIdItem(id) {
     return await prismaClient.item.findUnique({
       where: { id },
-    });
+    })
   }
 
   async findAllItens({ skip = 0, take = 10 }) {
@@ -35,8 +46,10 @@ export class ItemRepository {
         contato: true,
         status: true,
         foto: true,
+        usuarioId: true, // Adicionar o ID do usuário
         user: {
           select: {
+            id: true, // Adicionar o ID do usuário
             name: true,
             email: true,
             phone: true,
@@ -49,27 +62,49 @@ export class ItemRepository {
           },
         },
       },
-    });
+    })
   }
 
   async updateItemById(id, updateData) {
     try {
+      // Garantir que a data seja um objeto Date válido
+      if (updateData.data && typeof updateData.data === "string") {
+        updateData.data = new Date(updateData.data)
+      }
+
       return await prismaClient.item.update({
         where: { id },
         data: updateData,
-      });
+      })
     } catch (error) {
-      throw new Error("Erro ao atualizar item no banco de dados");
+      console.error("Erro ao atualizar item no repositório:", error)
+      throw error
     }
   }
 
   async findById(id) {
     return await prismaClient.item.findUnique({
       where: { id },
-    });
+      include: {
+        user: {
+          select: {
+            id: true, // Adicionar o ID do usuário
+            name: true,
+            email: true,
+            phone: true,
+          },
+        },
+        categoria: {
+          select: {
+            name: true,
+            id: true,
+          },
+        },
+      },
+    })
   }
 
   async countItens() {
-    return await prismaClient.item.count();
+    return await prismaClient.item.count()
   }
 }
